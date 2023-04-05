a2ea45919718b083a6deeac08f6b6155afe15fa0
Trever Taylor <treverjtaylor1@gmail.com>
2023-04-04 21:24:22 -0500

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,2 @@
-import emailjs from 'emailjs-com';
-import { useRef, useState, useEffect } from 'react';
-import thankyou from '../scripts/email';
 
-export default function ContactForm(){
-    return(
-        
-        
-    )
-
-}
 
