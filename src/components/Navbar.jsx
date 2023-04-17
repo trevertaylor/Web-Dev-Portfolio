0b7afe807e0615e1752d1cb1f1104c77ea970c05
trevertaylor <126427176+trevertaylor@users.noreply.github.com>
2023-04-17 12:43:33 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,6 +54,7 @@ export default function Navbar(){
                                     </div>
                                 </div>
                             </div>
+                    </div>
             </div>
 </header>
             
